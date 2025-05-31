b5d5cc10686573900de4b2c7e1c7d0b8fffac40e
rizredvlo7zer0 <50101349+rizredvlo7zer0@users.noreply.github.com>
2025-06-01 00:49:56 +0700

    Update server.js

--- server.js
+++ server.js
@@ -3,10 +3,26 @@ const cors_proxy = require('cors-anywhere');
 const host = '0.0.0.0';
 const port = process.env.PORT || 8080;
 
-cors_proxy.createServer({
-  originWhitelist: [], // Allow all origins
+const server = cors_proxy.createServer({
+  originWhitelist: [], // Izinkan semua origin
   requireHeader: ['origin', 'x-requested-with'],
-  removeHeaders: ['cookie', 'cookie2']
-}).listen(port, host, () => {
-  console.log(`CORS Anywhere server running on http://${host}:${port}`);
+  removeHeaders: ['cookie', 'cookie2'],
+  
+  // Tambahkan handler untuk modifikasi request
+  modifyRequest: (req) => {
+    // Force Accept header untuk meminta JSON
+    req.headers['accept'] = 'application/json';
+    return req;
+  },
+  
+  // Tambahkan handler untuk modifikasi response
+  modifyResponse: (res) => {
+    // Pastikan Content-Type adalah JSON
+    res.headers['content-type'] = 'application/json';
+    return res;
+  }
+});
+
+server.listen(port, host, () => {
+  console.log(`Server CORS proxy berjalan di http://${host}:${port}`);
 });
